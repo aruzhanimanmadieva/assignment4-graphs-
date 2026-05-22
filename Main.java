e8c0055ac7705f23a6ef3ad69a8cdf4f303614ba
aruzhanimanmadieva <aruzhanimanmadieva@gmail.com>
2026-05-22 18:57:28 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,16 +5,16 @@ public class Main{
         for(int i=0;i<10;i++){
             graph.addVertex(new Vertex(i));
         }
-        // Add edges
-        graph.addEdge(0,1);
-        graph.addEdge(0,2);
-        graph.addEdge(1,3);
-        graph.addEdge(1,4);
-        graph.addEdge(2,5);
-        graph.addEdge(2,6);
-        graph.addEdge(3,7);
-        graph.addEdge(4,8);
-        graph.addEdge(5,9);
+        // Add weighted edges
+        graph.addWeightedEdge(0,1,4);
+        graph.addWeightedEdge(0,2,2);
+        graph.addWeightedEdge(1,3,5);
+        graph.addWeightedEdge(1,4,1);
+        graph.addWeightedEdge(2,5,3);
+        graph.addWeightedEdge(2,6,6);
+        graph.addWeightedEdge(3,7,2);
+        graph.addWeightedEdge(4,8,7);
+        graph.addWeightedEdge(5,9,4);
         //Print graph
         System.out.println("GRAPH STRUCTURE");
         graph.printGraph();
@@ -22,6 +22,7 @@ public class Main{
         System.out.println();
         graph.bfs(0);
         graph.dfs(0);
+        graph.dijkstra(0);
         //Experiments
         Experiment experiment=new Experiment();
         experiment.runMultipleTests();
